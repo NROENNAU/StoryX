f44714aa21803cfcac0f9014c05852dbeea9884b
NROENNAU <n.roennau@gmx.net>
2024-11-03 10:14:05 +0100

    Update StoryX.js

--- StoryX.js
+++ StoryX.js
@@ -70,7 +70,7 @@ function loadUserGroupAndStories() {
                 goToRandomStory(); // Zufällige Geschichte anzeigen
             }).catch((error) => console.error("Fehler beim Abrufen der Gruppendaten: ", error));
         } else {
-            document.getElementById("currentGroupName").innerText = "Keine Gruppe";
+            document.getElementById("currentGroupName").innerText = "Get started 🟢";
         }
     }).catch((error) => console.error("Fehler beim Abrufen der Gruppeninformationen: ", error));
 }
